80b3efc0cc8821ab41efba0efa5b5e7793ba5558
poonamsumedh2 <50121869+poonamsumedh2@users.noreply.github.com>
2019-09-26 17:25:12 -0400

    Add files via upload

---
+++ World_IncomeLevel/data.js
@@ -0,0 +1,113 @@
+var data = [{
+
+    "Country Code": "USA",
+    "Country Name": "United States",
+    "region": "North America",
+    "income_level": "High income",
+    "latitude": 38.8895,
+    "longitude": -77.032,
+    "2018_gdp": 20500000000000,
+    "2018_pop": 327167434,
+    "GDP Per Capita": 62659.04814963949
+},
+{
+    "Country Code": "CHN",
+    "Country Name": "China",
+    "region": "East Asia & Pacific",
+    "income_level": "Upper middle income",
+    "latitude": 40.0495,
+    "longitude": 116.286,
+    "2018_gdp": 13600000000000,
+    "2018_pop": 1392730000,
+    "GDP Per Capita": 9764.993932779505
+},
+{
+    "Country Code": "JPN",
+    "Country Name": "Japan",
+    "region": "East Asia & Pacific",
+    "income_level": "High income",
+    "latitude": 35.67,
+    "longitude": 139.77,
+    "2018_gdp": 4970000000000,
+    "2018_pop": 126529100,
+    "GDP Per Capita": 39279.501711464
+},
+{
+    "Country Code": "DEU",
+    "Country Name": "Germany",
+    "region": "Europe & Central Asia",
+    "income_level": "High income",
+    "latitude": 52.5235,
+    "longitude": 13.4115,
+    "2018_gdp": 4000000000000,
+    "2018_pop": 82927922,
+    "GDP Per Capita": 48234.65852671432
+},
+{
+    "Country Code": "GBR",
+    "Country Name": "United Kingdom",
+    "region": "Europe & Central Asia",
+    "income_level": "High income",
+    "latitude": 51.5002,
+    "longitude": -0.126236,
+    "2018_gdp": 2830000000000,
+    "2018_pop": 66488991,
+    "GDP Per Capita": 42563.43730648582
+},
+{
+    "Country Code": "FRA",
+    "Country Name": "France",
+    "region": "Europe & Central Asia",
+    "income_level": "High income",
+    "latitude": 48.8566,
+    "longitude": 2.35097,
+    "2018_gdp": 2780000000000,
+    "2018_pop": 66987244,
+    "GDP Per Capita": 41500.438501395874
+},
+{
+    "Country Code": "IND",
+    "Country Name": "India",
+    "region": "South Asia",
+    "income_level": "Lower middle income",
+    "latitude": 28.6353,
+    "longitude": 77.225,
+    "2018_gdp": 2730000000000,
+    "2018_pop": 1352617328,
+    "GDP Per Capita": 2018.3092020835031
+},
+{
+    "Country Code": "ITA",
+    "Country Name": "Italy",
+    "region": "Europe & Central Asia",
+    "income_level": "High income",
+    "latitude": 41.8955,
+    "longitude": 12.4823,
+    "2018_gdp": 2070000000000,
+    "2018_pop": 60431283,
+    "GDP Per Capita": 34253.782101564844
+},
+{
+    "Country Code": "BRA",
+    "Country Name": "Brazil",
+    "region": "Latin America & Caribbean",
+    "income_level": "Upper middle income",
+    "latitude": -15.7801,
+    "longitude": -47.9292,
+    "2018_gdp": 1870000000000,
+    "2018_pop": 209469333,
+    "GDP Per Capita": 8927.321117693156
+},
+{
+    "Country Code": "CAN",
+    "Country Name": "Canada",
+    "region": "North America",
+    "income_level": "High income",
+    "latitude": 45.4215,
+    "longitude": -75.6919,
+    "2018_gdp": 1710000000000,
+    "2018_pop": 37058856,
+    "GDP Per Capita": 46142.81671296059
+}
+
+];
